2c6f80a74f8ca33e2afdc32c4ff3be77db5aa8a8
ponkey364 <ponkey364@outlook.com>
2021-09-23 21:15:03 +0100

    add an event to ChannelListBloc for the homepage query

--- lib/blocs/repos/channel_list_bloc.dart
+++ lib/blocs/repos/channel_list_bloc.dart
@@ -32,6 +32,14 @@ class LoadMyLiveFollowedChannels extends ChannelListEvent {
   List<Object> get props => [];
 }
 
+class LoadHomepageChannels extends ChannelListEvent {
+  @override
+  String toString() => 'LoadHomepageChannels';
+
+  @override
+  List<Object> get props => [];
+}
+
 @immutable
 abstract class ChannelListState extends Equatable {
   ChannelListState([List props = const []]) : super();
@@ -82,6 +90,8 @@ class ChannelListBloc extends Bloc<ChannelListEvent, ChannelListState> {
         yield* _loadChannels(event.categorySlug, event.channelLimit);
       } else if (event is LoadMyLiveFollowedChannels) {
         yield* _loadMyLiveFollowedChannels();
+      } else if (event is LoadHomepageChannels) {
+        yield* _loadHomepageChannels();
       } else {
         // New event, who dis?
       }
@@ -141,6 +151,35 @@ class ChannelListBloc extends Bloc<ChannelListEvent, ChannelListState> {
       yield ChannelListLoaded(results: listOfChannels);
     } catch (error) {
       print(error);
+      yield ChannelListNotLoaded();
+    }
+  }
+
+  Stream<ChannelListState> _loadHomepageChannels() async* {
+    try {
+      yield ChannelListLoading();
+
+      final queryResults = await this.glimeshRepository.getHomepageChannels();
+
+      if (queryResults.hasException) {
+        yield ChannelListNotLoaded(queryResults.exception!.graphqlErrors);
+        return;
+      }
+
+      final List<dynamic> channels =
+          queryResults.data!['homepageChannels']['edges'] as List<dynamic>;
+
+      // filter and then map here because channels can stop streaming and still be on the homepage,
+      // and this seemed like the best way instead of introducing nulls elsewhere.
+      final List<Channel> listOfChannels = channels
+          .where((c) => c['node']['stream'] != null)
+          .map(buildChannelFromJson)
+          .toList();
+
+      yield ChannelListLoaded(results: listOfChannels);
+    } catch (error, s) {
+      print(error);
+      print(s);
       yield ChannelListNotLoaded();
     }
   }
