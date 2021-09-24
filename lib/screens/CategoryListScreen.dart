335e1028ecf779c9d0253078cd4130603eafc891
ponkey364 <ponkey364@outlook.com>
2021-09-24 21:44:39 +0100

    copy across a _buildCard from the other pages

--- lib/screens/CategoryListScreen.dart
+++ lib/screens/CategoryListScreen.dart
@@ -190,4 +190,47 @@ class CategoryListWidget extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildCard(BuildContext context, Channel channel) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          height: 220.0,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                // In order to have the ink splash appear above the image, you
+                // must use Ink.image. This allows the image to be painted as part
+                // of the Material and display ink effects above it. Using a
+                // standard Image will obscure the ink splash.
+                child: Ink.image(
+                  image: NetworkImage(channel.thumbnail),
+                  fit: BoxFit.cover,
+                  child: Container(),
+                ),
+              ),
+              Positioned(
+                bottom: 16.0,
+                left: 16.0,
+                right: 16.0,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.black54,
+                    ),
+                    padding: EdgeInsets.all(15),
+                    child: Text(channel.title),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
