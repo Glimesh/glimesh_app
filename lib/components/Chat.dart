5d6f43a6ccb61120c9478bfe09b915cc579e308b
ponkey364 <ponkey364@outlook.com>
2021-11-21 13:13:20 +0000

    build chat messages from tokens

--- lib/components/Chat.dart
+++ lib/components/Chat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:glimesh_app/blocs/repos/chat_messages_bloc.dart';
 import 'package:glimesh_app/models.dart';
 
@@ -133,13 +134,52 @@ class Chat extends StatelessWidget {
                 ),
               ),
               TextSpan(
-                text: message.username + ": " + message.message,
+                text: message.username + ": ",
                 style: TextStyle(fontSize: 16),
-              )
+              ),
+              ..._buildTokens(message.tokens)
             ]),
           ),
         ),
       ),
+    );
+  }
+
+  final double smallEmoteSize = 20;
+  final double bigEmoteSize = 64;
+
+  List<InlineSpan> _buildTokens(List<MessageToken> tokens) {
+    return tokens.map((token) {
+      if (token.tokenType == "emote" && token.src != null) {
+        // on the web, if an message is just an emote, we make it bigger, let's do that here too!
+        var emoteSize = tokens.length == 1 ? bigEmoteSize : smallEmoteSize;
+
+        return WidgetSpan(
+            child: Padding(
+                child: SizedBox(child: _drawEmote(token), width: emoteSize),
+                padding: EdgeInsets.symmetric(horizontal: 4)));
+      }
+
+      // we don't know the token type, or it's just text, just return the text for now.
+      return TextSpan(
+        text: token.text,
+        style: TextStyle(fontSize: 16),
+      );
+    }).toList();
+  }
+
+  // due to restrictions with flutter's selection of SVG libraries, we're having to use png
+  // emotes, which, given we never show them above text size 64, shouldn't *really* be an issue
+  Widget _drawEmote(MessageToken token) {
+    var url = token.src!;
+
+    if (token.src!.contains(".svg")) {
+      url = token.src!.replaceAll(".svg", ".png");
+    }
+
+    return Image(
+      image: CachedNetworkImageProvider(url),
+      filterQuality: FilterQuality.medium,
     );
   }
 }
