75290468533964708bd51b8ccc3c7267ce16fdb1
Luke Strickland <luke@axxim.net>
2022-03-22 14:36:34 -0400

    Adding error handling if video hasn't loaded in 10 seconds

--- lib/components/FTLPlayer.dart
+++ lib/components/FTLPlayer.dart
@@ -30,6 +30,8 @@ class _FTLPlayerState extends State<FTLPlayer> {
 
   bool _loading = true;
   bool _errored = false;
+  bool _fatalErrored = false;
+  int _seconds = 0;
 
   watchChannel(int channelId) {
     plugin!.send(data: {"request": "watch", "channelId": channelId});
@@ -105,6 +107,7 @@ class _FTLPlayerState extends State<FTLPlayer> {
           setState(() {
             _errored = false;
             _loading = false;
+            _seconds = 0;
           });
         } catch (error) {
           // We can likely retry loading this stream with initJanusClient();
@@ -115,6 +118,7 @@ class _FTLPlayerState extends State<FTLPlayer> {
             setState(() {
               _errored = true;
               _loading = false;
+              _seconds = 0;
             });
           }
           return;
@@ -122,13 +126,35 @@ class _FTLPlayerState extends State<FTLPlayer> {
       }
     });
 
-    // Pathetic excuse for error handling...
-    Timer.periodic(Duration(seconds: 1), (timer) async {
+    Timer.periodic(Duration(seconds: 1), (innerTimer) async {
+      if (mounted == false) {
+        return;
+      }
+
+      // if we don't have a videoHeight after 10 seconds, assume we errored due to VPN or similar.
+      setState(() {
+        _seconds += 1;
+      });
+      if (_seconds > 10 && _remoteRenderer.videoHeight == 0) {
+        print("Video loading for greater than 10 seconds, aborting");
+        await Sentry.captureMessage(
+            "Video loading for greater than 10 seconds, aborting");
+
+        innerTimer.cancel();
+        stopVideo();
+
+        setState(() {
+          _fatalErrored = true;
+          _loading = false;
+        });
+      }
+
+      // Check to see if Janus lost connection without telling us
       if (plugin != null && plugin!.pollingActive == false) {
         print("Polling has failed without telling us, resetting the widget");
         await Sentry.captureMessage(
             "Polling has failed without telling us, resetting the widget");
-        timer.cancel();
+        innerTimer.cancel();
 
         if (mounted) {
           // If we're not mounted, we can just forget about it
@@ -153,9 +179,7 @@ class _FTLPlayerState extends State<FTLPlayer> {
     }
 
     if (plugin != null) {
-      plugin!.send(data: {"request": "stop"});
-      plugin!.remoteStream = null;
-      plugin!.dispose();
+      stopVideo();
     }
 
     if (session != null) {
@@ -168,26 +192,45 @@ class _FTLPlayerState extends State<FTLPlayer> {
     super.dispose();
   }
 
+  void stopVideo() {
+    plugin!.send(data: {"request": "stop"});
+    plugin!.remoteStream = null;
+    plugin!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Background layers that the video will take over when properly loaded
-        Image.network(widget.channel.thumbnail),
-        Container(
-          decoration: BoxDecoration(color: Colors.black45),
-          child: Loading("Loading Video"),
-        ),
-        _errored
-            ? Center(
-                child: Text("Error loading video, please try again."),
-              )
-            : RTCVideoView(
-                _remoteRenderer,
-                objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitContain,
+    return _fatalErrored
+        ? Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Text(
+                "Error loading video, please disable any VPNs or other software that could negatively impact WebRTC video and then try again.",
+                textAlign: TextAlign.center,
               ),
-        _loading ? Loading("Loading Video") : Padding(padding: EdgeInsets.zero),
-      ],
-    );
+            ),
+          )
+        : Stack(
+            children: [
+              // Background layers that the video will take over when properly loaded
+              Image.network(widget.channel.thumbnail),
+              Container(
+                decoration: BoxDecoration(color: Colors.black45),
+                child: Loading("Loading Video"),
+              ),
+              _errored
+                  ? Center(
+                      child: Text("Error loading video, please try again."),
+                    )
+                  : RTCVideoView(
+                      _remoteRenderer,
+                      objectFit:
+                          RTCVideoViewObjectFit.RTCVideoViewObjectFitContain,
+                    ),
+              _loading
+                  ? Loading("Loading Video")
+                  : Padding(padding: EdgeInsets.zero),
+            ],
+          );
   }
 }
